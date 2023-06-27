a75118aa2f3cd5ba12ff19a5876b9dd41275f216
rodiongavrilov <rodiongavrilov@gmail.com>
2023-06-27 22:50:33 +0300

    fix: express js

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ const cron = require('node-cron');
 
 const app = express();
 
-const sendDiscordMessage = async (message) => {
+const joke = ''
+const getJoke = async () => {
     try {
-        await axios.post('https://discord.com/api/webhooks/1089610761585819739/o96xVqP5wjvJRJL3H9S8PLNldnl0FTqBtps9i9Nc2-n3jkl2Uuy5QSeOQk1MdTeYVXlT', { content: message });
+        const response = await axios.get('https://backend-omega-seven.vercel.app/api/getjoke\n');
+        this.joke = `\nJOKE! \n Question:\n——${response.data[0].question}\nAnswer:\n——${response.data[0].punchline}`;
+    } catch (error) {
+        console.error('Ошибка при получении шутки:', error);
+    }
+}
+const sendDiscordMessage = async () => {
+    try {
+        await getJoke();
+        await axios.post('https://discord.com/api/webhooks/1089610761585819739/o96xVqP5wjvJRJL3H9S8PLNldnl0FTqBtps9i9Nc2-n3jkl2Uuy5QSeOQk1MdTeYVXlT', { content: this.joke });
         console.log('Сообщение успешно отправлено в Discord');
     } catch (error) {
         console.error('Ошибка при отправке сообщения в Discord:', error);
@@ -18,15 +28,6 @@ app.get('/send-message', (req, res) => {
     sendDiscordMessage(message);
     res.send('Сообщение отправлено в Discord');
 });
-setInterval(() => {
-    axios.get('http://localhost:3000/send-message')
-        .then(response => {
-            console.log(response.data);
-        })
-        .catch(error => {
-            console.error('Ошибка при выполнении задачи:', error);
-        });
-}, 10000)
 cron.schedule('* * * * *', () => {
     axios.get('http://localhost:3000/send-message')
         .then(response => {
