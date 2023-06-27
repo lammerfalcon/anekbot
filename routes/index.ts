01200105984711a8f4c141d4bad8e49ef632bf98
rodiongavrilov <rodiongavrilov@gmail.com>
2023-06-27 20:31:11 +0300

    fix: create plugin

--- routes/index.ts
+++ routes/index.ts
@@ -1,4 +1,11 @@
+import axios from "axios/index";
+
 export default eventHandler(async () => {
+  const url = 'https://discord.com/api/webhooks/1089610761585819739/o96xVqP5wjvJRJL3H9S8PLNldnl0FTqBtps9i9Nc2-n3jkl2Uuy5QSeOQk1MdTeYVXlT'
+
+  axios.post(url, {
+    content: 'test',
+  })
   return {
     statusCode: 200,
     body: 'Анекдоты успешно отправляются в Discord.',
