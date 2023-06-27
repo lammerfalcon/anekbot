207a71f3c209abf21da768f757aed0ff01200d07
rodiongavrilov <rodiongavrilov@gmail.com>
2023-06-27 20:33:44 +0300

    fix: create plugin

--- routes/index.ts
+++ routes/index.ts
@@ -1,4 +1,4 @@
-import axios from "axios/index";
+import axios from "axios";
 
 export default eventHandler(async () => {
   const url = 'https://discord.com/api/webhooks/1089610761585819739/o96xVqP5wjvJRJL3H9S8PLNldnl0FTqBtps9i9Nc2-n3jkl2Uuy5QSeOQk1MdTeYVXlT'
