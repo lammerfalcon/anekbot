cc921409f561fa1f8463e3b8654d24c08d282bf5
rodiongavrilov <rodiongavrilov@gmail.com>
2023-06-29 15:35:53 +0300

    fix: nitro

--- plugins/jokePlugin.ts
+++ plugins/jokePlugin.ts
@@ -1,39 +1,39 @@
 import axios from "axios";
 import * as cron from "node-cron";
-import {openai} from "../openAi";
-export default defineNitroPlugin(async (nitroApp) => {
+import { openai } from "../openAi";
 
+export default defineNitroPlugin(async () => {
+    async function getJoke() {
+        const prompt =
+            "Напиши анекдот про разработчиков(про бекендера или фронтендера или девопса или тестировщкиа или всех вместе или по парам, без разницы, тематика шутко должна быть про разработку )(ищи анекдоты которые хорошо транспонируются на русский язык - твой перевод не всегда корректно на русской речи выглядит)";
+        const maxTokens = 1000;
+        const temperature = 1;
 
-    // console.log(nitroApp)
-    // async function getJoke() {
-    //     const prompt = "Напиши анекдот про разработчиков(про бекендера или фронтендера или девопса или тестировщкиа или всех вместе или по парам, без разницы, тематика шутко должна быть про разработку )(ищи анекдоты которые хорошо транспонируются на русский язык - твой перевод не всегда корректно на русской речи выглядит)"; // Начальный текст для получения анекдота
-    //     const maxTokens = 1000; // Максимальное количество токенов в ответе (длина анекдота)
-    //     const temperature = 1; // Коэффициент разнообразия (чем выше, тем более случайный будет ответ)
-    //
-    //     const response = await openai.createCompletion({
-    //         model: 'text-davinci-003',
-    //         prompt: prompt,
-    //         max_tokens: maxTokens,
-    //         temperature: temperature,
-    //         n: 1,
-    //     });
-    //     console.log('inside')
-    //     const joke = '```' + response.data.choices[0].text.trim() + '```';
-    //     useStorage().setItem('joke',  joke )
-    //     return '```' + response.data.choices[0].text.trim() + '```';
-    // }
-    // const getAndSendJoke = async () => {
-    //     console.log('here')
-    //     await getJoke();
-    //     await axios.get('http://localhost:3000/')
-    // }
-    setInterval( () => {
-        axios.get('http://localhost:3000')
-    }, 3000)
-    // cron.schedule('* * * * *', async () => {
-    //     // await getAndSendJoke()
-    //     await useStorage().setItem('joke',  Math.random().toString() )
-    //     await axios.get('http://localhost:3000/')
-    //
-    // })
-})
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            max_tokens: maxTokens,
+            temperature: temperature,
+            n: 1,
+        });
+
+        const joke = "```" + response.data.choices[0].text.trim() + "```";
+        return joke;
+    }
+
+    const getAndSendJoke = async () => {
+        const joke = await getJoke();
+        await axios.post(process.env.DISCORD_WEBHOOK, {
+            content: joke,
+        });
+    };
+
+    const sendJokePeriodically = async () => {
+        while (true) {
+            await getAndSendJoke();
+            await new Promise((resolve) => setTimeout(resolve, 13000));
+        }
+    };
+
+    sendJokePeriodically();
+});
--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
-import {nitroPlugin} from "#imports";
 
 export default eventHandler(async () => {
     const joke = await useStorage().getItem('joke')
         await axios.post(process.env.DISCORD_WEBHOOK, {
-            content: 'test'
+            content: joke,
         })
   return { status: 200, message: {text: 'Анекдот отправлен'} }
 })
