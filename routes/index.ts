2c175d0c526316185039125d0a1bfafdffa0ccee
rodiongavrilov <rodiongavrilov@gmail.com>
2023-06-29 14:09:34 +0300

    fix: nitro

--- routes/index.ts
+++ routes/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export default eventHandler(async () => {
     const joke = await useStorage().getItem('joke')
         await axios.post(process.env.DISCORD_WEBHOOK, {
-            content: joke || 'Анекдот не найден'
+            content: 'Анекдот не найден'
         })
   return { status: 200, message: {text: 'Анекдот отправлен'} }
 })
