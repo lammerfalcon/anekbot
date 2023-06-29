aff2b217a2fb0bcb75c2e04f4fefbb87e44f470c
rodiongavrilov <rodiongavrilov@gmail.com>
2023-06-29 17:59:42 +0300

    fix: nitro

--- plugins/jokePlugin.ts
+++ plugins/jokePlugin.ts
@@ -3,13 +3,8 @@ import * as cron from "node-cron";
 import { openai } from "../openAi";
 
 export default defineNitroPlugin(async () => {
-    await axios.post(process.env.DISCORD_WEBHOOK, {
-        content: 'init plugin',
-    });
+
     async function getJoke() {
-        await axios.post(process.env.DISCORD_WEBHOOK, {
-            content: 'get joke start',
-        });
         const prompt =
             "Напиши анекдот про разработчиков(про бекендера или фронтендера или девопса или тестировщкиа или всех вместе или по парам, без разницы, тематика шутко должна быть про разработку )(ищи анекдоты которые хорошо транспонируются на русский язык - твой перевод не всегда корректно на русской речи выглядит)";
         const maxTokens = 1000;
@@ -22,21 +17,14 @@ export default defineNitroPlugin(async () => {
             temperature: temperature,
             n: 1,
         });
-        await axios.post(process.env.DISCORD_WEBHOOK, {
-            content: 'joke getted',
-        });
+
         const joke = "```" + response.data.choices[0].text.trim() + "```";
         return joke;
     }
 
     const getAndSendJoke = async () => {
-        await axios.post(process.env.DISCORD_WEBHOOK, {
-            content: 'inside get and send joke',
-        });
         await getJoke().then((joke) => {
-            axios.post(process.env.DISCORD_WEBHOOK, {
-                content: 'joke sended',
-            });
+
             axios.post(process.env.DISCORD_WEBHOOK, {
                 content: joke,
             });
@@ -44,5 +32,5 @@ export default defineNitroPlugin(async () => {
 
     };
     getAndSendJoke();
-setInterval(getAndSendJoke, 1000 * 10);
+setInterval(getAndSendJoke, 1000 * 60);
 });
