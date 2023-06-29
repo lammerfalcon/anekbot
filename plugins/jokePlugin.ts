c97b16661b280f341b14073a8f01e0b3f685344a
rodiongavrilov <rodiongavrilov@gmail.com>
2023-06-29 14:47:21 +0300

    fix: nitro

--- plugins/jokePlugin.ts
+++ plugins/jokePlugin.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 import * as cron from "node-cron";
 import {openai} from "../openAi";
+export default defineNitroPlugin(async (nitroApp) => {
 
-export default defineNitroPlugin(async () => {
 
+    // console.log(nitroApp)
     // async function getJoke() {
     //     const prompt = "Напиши анекдот про разработчиков(про бекендера или фронтендера или девопса или тестировщкиа или всех вместе или по парам, без разницы, тематика шутко должна быть про разработку )(ищи анекдоты которые хорошо транспонируются на русский язык - твой перевод не всегда корректно на русской речи выглядит)"; // Начальный текст для получения анекдота
     //     const maxTokens = 1000; // Максимальное количество токенов в ответе (длина анекдота)
@@ -27,8 +28,8 @@ export default defineNitroPlugin(async () => {
     //     await axios.get('http://localhost:3000/')
     // }
     setInterval( () => {
-        axios.get('http://localhost:3000/')
-    }, 5000)
+        axios.get('http://localhost:3000')
+    }, 3000)
     // cron.schedule('* * * * *', async () => {
     //     // await getAndSendJoke()
     //     await useStorage().setItem('joke',  Math.random().toString() )
